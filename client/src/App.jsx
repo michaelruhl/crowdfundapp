ba031d38bb4f5bfd26a7e1402a61135cab7258d0
Michael Ruhl <michaelruhl14@hotmail.com>
2023-07-27 18:26:21 -0400

    implement CreateCampaign logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,11 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+
+
         </Routes>
       </div>
     </div>
---
+++ client/src/context/index.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  // address of smart contract
+  const { contract } = useContract(
+    "0x7E23225D524f0103947791723E84A33Fb4705825"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign({
+				args: [
+					address, // owner
+					form.title, // title
+					form.description, // description
+					form.target,
+					new Date(form.deadline).getTime(), // deadline,
+					form.image,
+				],
+			});
+
+      console.log("contract call success", data)
+    } catch (error) {
+      console.log("contract call failure", error)
+    }
+  }
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
